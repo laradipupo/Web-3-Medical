c55c57b18da012af2ec88f17d37fa6fe1bf3c0c9
winifredogbeiwi <imadeogbeiwi@gmail.com>
2023-05-09 17:57:24 +0100

    navigation Bar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { BsChatDots } from 'react-icons/bs'
+import { useLocation, useNavigate } from 'react-router-dom'
+const Navbar = () => {
+    const location = useLocation()
+    const pathRoute = (route) => {
+        if (route === location.pathname) {
+            return true
+        }
+    }
+    const navigate = useNavigate()
+    return (
+        <>
+            <nav className='flex justify-between items-center pt-5'>
+                <div>
+                    <ul className='flex items-center space-x-9 font-bold text-sm '>
+                        <li className={`cursor-pointer text-base text-black ${pathRoute('/') && '!text-main-color'}`} onClick={() => navigate('/')}>MEDICAL</li>
+                        <li className={`cursor-pointer text-black ${pathRoute('/about') && '!text-main-color'}`} onClick={() => navigate('/about')}>About Us </li>
+                        <li>For Doctors</li>
+                        <li>For Patient</li>
+                        <li>Pricing</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul className='flex items-center space-x-9 font-bold text-sm'>
+                        <li>Help</li>
+                        <li>Login</li>
+                        <button className=' text-white bg-main-color py-[0.875rem] px-8 rounded-lg'>Get Started</button>
+                        <button className=' text-white bg-main-color py-[0.875rem] px-7 rounded-lg text-lg'> <BsChatDots /></button>
 
-function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-color": "#9974FF",
+      },
+    },
   },
   plugins: [],
 };
-
