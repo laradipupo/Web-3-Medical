008c7fcd7e52ca8698b68ea7f768a4fdb6a090c9
laradipupo <omolaraoladipupo11@gmail.com>
2023-05-19 10:05:18 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./layout/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Doctors from "./pages/Doctors";
 import Footer from "./layout/Footer";
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/doctors" element={<Doctors />} />
         </Routes>
         <Footer/>
       </BrowserRouter>
